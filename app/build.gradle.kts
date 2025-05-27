1606da7b5a78d33699ddb8fe36f6b51659bbdb8f
leo <leonardoalvarez1118@gmail.com>
2025-05-27 00:24:47 -0500

    creacion UI login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -33,15 +34,23 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    viewBinding{
+        enable=true
+    }
 }
 
 dependencies {
+    implementation(platform(libs.firebase.bom))
+
+    implementation(libs.firebase.analytics)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
